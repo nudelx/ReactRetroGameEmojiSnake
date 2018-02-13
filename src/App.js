7458bce3875a0c2aee75f5d26411a966801e02d8
nudelx <alex.nudelman@samanage.com>
2018-02-13 14:34:08 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
+import ReactHeader from './components/recatHeader'
 import SnakeGame from './games/snakeGame'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
+        <ReactHeader />
         <SnakeGame />
+
+        {/* and more games here  */}
       </div>
     )
   }
