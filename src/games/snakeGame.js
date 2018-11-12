db9b4ec053803e32c9fac1e50fceb7cc3cb9e75d
Alex Nudelman <nudelx@gmail.com>
2018-11-12 21:37:56 +0200

    split code

--- src/games/snakeGame.js
+++ src/games/snakeGame.js
@@ -7,30 +7,34 @@ import withBGM from "../HOC/playerWithBGM"
 import SoundBGMControl from "../components/bgmControl"
 import GameRun from "../components/GameRun"
 import { useGameState } from "../hooks/gameState"
+import { useBorardHook } from "../hooks/boardHook"
+import Loading from "../components/loading"
 
 const SoundPlayerBGM = withBGM(SoundPlayer)
 
 const SnakeGame = () => {
   const {
+    gameFail,
+    playSound,
+    setPlaySound,
+    gameRunning,
+    score,
+    setScore,
+    playBGM,
+    toggleGameFail,
+    startTheGame,
+    closePlayer,
+    togglePlayBGM
+  } = useGameState()
+  const {
+    width,
+    height,
     foodX,
     foodY,
     foodVisible,
-    score,
-    playSound,
-    gameRunning,
-    gameFail,
-    playBGM,
-    width,
-    height,
-    step,
     toggleFood,
-    setScore,
-    setPlaySound,
-    toggleGameFail,
-    startTheGame,
-    togglePlayBGM,
-    closePlayer
-  } = useGameState()
+    step
+  } = useBorardHook()
   const gameRunProps = {
     width,
     height,
@@ -46,18 +50,16 @@ const SnakeGame = () => {
     foodVisible,
     playSound
   }
-  console.log("width", width)
-  console.log("height", height)
+
   return (
     <div>
-      <Board width={width} height={height}>
+      <Board width={width} height={height} fallback={Loading}>
         {gameRunning && !gameFail ? (
           <GameRun {...gameRunProps} />
         ) : gameFail ? (
           <GameOver score={score} onClick={startTheGame} />
         ) : (
           <StartButton onClick={startTheGame} />
-        )}
         )}
       </Board>
       {playBGM && <SoundPlayerBGM />}
